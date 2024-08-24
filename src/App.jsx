0bd918787ff1907d4349dee97f2c4ca72a8bf3b3
Sudip Bose <sbose007ime.work@gmail.com>
2024-08-24 22:36:45 +0530

    Refactor: Codebase location of static file dir and styles dir

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import "./styles/App.css";
+import "../styles/App.css";
 
 import Navbar from "./components/Navbar";
 import Footer from './components/Footer';
