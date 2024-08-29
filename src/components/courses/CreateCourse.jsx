0aca5bd3ee7fdd3021e2d2b265842da86afe29cc
Sudip Bose <sbose007ime.work@gmail.com>
2024-08-29 13:11:02 +0530

    Update: Create fields formatting issue

--- src/components/courses/CreateCourse.jsx
+++ src/components/courses/CreateCourse.jsx
@@ -1,100 +1,101 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 import Button from "@mui/material/Button";
 import AddIcon from "@mui/icons-material/Add";
 
-
 function CreateCourse() {
-    const [courseTitle, setCourseTitle] = useState('');
-    const [courseCode, setCourseCode] = useState('');
-    const [courseDescription, setCourseDescription] = useState('');
-    const [error, setError] = useState('');
-    const [success, setSuccess] = useState('');
+  const [courseTitle, setCourseTitle] = useState("");
+  const [courseCode, setCourseCode] = useState("");
+  const [courseDescription, setCourseDescription] = useState("");
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
+  const handleSubmit = (event) => {
+    event.preventDefault();
 
-        // Simple input validation
-        if (!courseTitle || !courseCode || !courseDescription) {
-            setError('All fields are required.');
-            setSuccess('');
-            return;
+    // Simple input validation
+    if (!courseTitle || !courseCode || !courseDescription) {
+      setError("All fields are required.");
+      setSuccess("");
+      return;
+    }
+    const newCourse = { courseTitle, courseCode, courseDescription };
+    fetch("http://localhost:8080/api/v1/courses", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newCourse),
+    })
+      .then(async (response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          const errorData = await response.json();
+          throw new Error(errorData.message);
         }
-        const newCourse = {courseTitle, courseCode, courseDescription};
-        fetch('http://localhost:8080/api/v1/courses', { 
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(newCourse),
-        })
-        .then(async response => {
-            if (response.ok) {
-                return response.json();
-            } else {
-                const errorData = await response.json();
-                throw new Error(errorData.message);
-            }
-        })
-        .then(data => {
-            setError('');
-            setSuccess('Course created');
-            setTimeout(() => setSuccess(''), 4000);
-            setCourseTitle('');
-            setCourseCode('');
-            setCourseDescription('');
-        })
-        .catch(error => {
-            console.error(error);
-            setError(error.message);
-            setTimeout(() => setError(''), 5000);
-            setSuccess('');
-        });
-    };
+      })
+      .then((data) => {
+        setError("");
+        setSuccess("Course created");
+        setTimeout(() => setSuccess(""), 4000);
+        setCourseTitle("");
+        setCourseCode("");
+        setCourseDescription("");
+      })
+      .catch((error) => {
+        console.error(error);
+        setError(error.message);
+        setTimeout(() => setError(""), 5000);
+        setSuccess("");
+      });
+  };
 
-    return (
+  return (
     <>
-        <div>
-            {error && <div style={{ color: 'red' }}>{error}</div>}
-            {success && <div style={{ color: 'green' }}>{success}</div>}
-            <form onSubmit={handleSubmit}>
-                <div className="input-container">
-                    <div>
-                        <input
-                            type="text"
-                            value={courseTitle}
-                            onChange={(e) => setCourseTitle(e.target.value)}
-                            placeholder="Course Title"
-                            required
-                            className="input-field"
-                        />
-                    </div>
-                    <div>
-                        <input
-                            type="text"
-                            value={courseCode}
-                            onChange={(e) => setCourseCode(e.target.value)}
-                            placeholder="Course Code"
-                            required
-                            className="input-field"
-                        />
-                    </div>
-                    <div>
-                        <input
-                            type="text"
-                            value={courseDescription}
-                            onChange={(e) => setCourseDescription(e.target.value)}
-                            placeholder="Course Description"
-                            required
-                            className="input-field"
-                        />
-                    </div>
-                    <Button type="submit" variant="outlined" startIcon={<AddIcon />}>Create</Button>
-                </div>
-            </form>
-        </div>
+      <div>
+        {error && <div style={{ color: "red" }}>{error}</div>}
+        {success && <div style={{ color: "green" }}>{success}</div>}
+        <form onSubmit={handleSubmit}>
+          <div className="input-container">
+            <div>
+              <input
+                type="text"
+                value={courseTitle}
+                onChange={(e) => setCourseTitle(e.target.value)}
+                placeholder="Course Title"
+                required
+                className="input-field"
+              />
+            </div>
+            <div>
+              <input
+                type="text"
+                value={courseCode}
+                onChange={(e) => setCourseCode(e.target.value)}
+                placeholder="Course Code"
+                required
+                className="input-field"
+              />
+            </div>
+            <div>
+              <input
+                type="text"
+                value={courseDescription}
+                onChange={(e) => setCourseDescription(e.target.value)}
+                placeholder="Course Description"
+                required
+                className="input-field"
+              />
+            </div>
+            <Button type="submit" variant="outlined" startIcon={<AddIcon />}>
+              Create
+            </Button>
+          </div>
+        </form>
+      </div>
     </>
-    );
+  );
 }
 
 export default CreateCourse;
--- src/components/delivery/CreateDelivery.jsx
+++ src/components/delivery/CreateDelivery.jsx
@@ -1,119 +1,121 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 import Button from "@mui/material/Button";
 import AddIcon from "@mui/icons-material/Add";
 
 function CreateDelivery() {
-    const [courseCode, setCourseCode] = useState('');
-    const [courseYear, setCourseYear] = useState('');
-    const [courseSemester, setCourseSemester] = useState('');
-    const [error, setError] = useState('');
-    const [success, setSuccess] = useState('');
+  const [courseCode, setCourseCode] = useState("");
+  const [courseYear, setCourseYear] = useState("");
+  const [courseSemester, setCourseSemester] = useState("");
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-    
-        // Simple input validation
-        if (!courseCode || !courseYear || !courseSemester) {
-            setError('All fields are required.');
-            setSuccess('');
-            return;
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    // Simple input validation
+    if (!courseCode || !courseYear || !courseSemester) {
+      setError("All fields are required.");
+      setSuccess("");
+      return;
+    }
+
+    // Check if the course exists
+    fetch(`http://localhost:8080/api/v1/courses/code/${courseCode}`)
+      .then((response) => {
+        if (response.status === 404) {
+          throw new Error("Course not found");
         }
-    
-        // Check if the course exists
-        fetch(`http://localhost:8080/api/v1/courses/code/${courseCode}`)
-            .then(response => {
-                if (response.status === 404) {
-                    throw new Error('Course not found');
-                }
-                return response.json();
-            })
-            .then(courseData => {
-                // If the course exists, proceed to create the course instance
-                const newCourseDelivery = {
-                    course: {
-                        id: courseData.data.id,
-                        courseTitle: courseData.data.courseTitle,
-                        courseCode: courseData.data.courseCode,
-                        courseDescription: courseData.data.courseDescription
-                    },
-                    year: courseYear,
-                    semester: courseSemester
-                };
-    
-                return fetch('http://localhost:8080/api/v1/instances', { 
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify(newCourseDelivery),
-                });
-            })
-            .then(async response => {
-                if (!response.ok) {
-                    const errorData = await response.json();
-                    throw new Error(errorData.message);
-                }
-                return response.json();
-            })
-            .then(data => {
-                setError('');
-                setSuccess('Course session created');
-                setTimeout(() => setSuccess(''), 4000);
-                setCourseCode('');
-                setCourseYear('');
-                setCourseSemester('');
-            })
-            .catch(error => {
-                setError(error.message);
-                setSuccess('');
-                setTimeout(() => setError(''), 5000);
-            });
-    };        
+        return response.json();
+      })
+      .then((courseData) => {
+        // If the course exists, proceed to create the course instance
+        const newCourseDelivery = {
+          course: {
+            id: courseData.data.id,
+            courseTitle: courseData.data.courseTitle,
+            courseCode: courseData.data.courseCode,
+            courseDescription: courseData.data.courseDescription,
+          },
+          year: courseYear,
+          semester: courseSemester,
+        };
 
-    return (
+        return fetch("http://localhost:8080/api/v1/instances", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(newCourseDelivery),
+        });
+      })
+      .then(async (response) => {
+        if (!response.ok) {
+          const errorData = await response.json();
+          throw new Error(errorData.message);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setError("");
+        setSuccess("Course session created");
+        setTimeout(() => setSuccess(""), 4000);
+        setCourseCode("");
+        setCourseYear("");
+        setCourseSemester("");
+      })
+      .catch((error) => {
+        setError(error.message);
+        setSuccess("");
+        setTimeout(() => setError(""), 5000);
+      });
+  };
+
+  return (
     <>
-        <div>
-            {error && <div style={{ color: 'red' }}>{error}</div>}
-            {success && <div style={{ color: 'green' }}>{success}</div>}
-            <form onSubmit={handleSubmit}>
-                <div className="input-container">
-                    <div>
-                        <input
-                            type="text"
-                            value={courseCode}
-                            onChange={(e) => setCourseCode(e.target.value)}
-                            placeholder="Course Code"
-                            required
-                            className="input-field"
-                        />
-                    </div>
-                    <div>
-                        <input
-                            type="text"
-                            value={courseYear}
-                            onChange={(e) => setCourseYear(e.target.value)}
-                            placeholder="Year"
-                            required
-                            className="input-field"
-                        />
-                    </div>
-                    <div>
-                        <input
-                            type="text"
-                            value={courseSemester}
-                            onChange={(e) => setCourseSemester(e.target.value)}
-                            placeholder="Semester"
-                            required
-                            className="input-field"
-                        />
-                    </div>
-                    <Button type="submit" variant="outlined" startIcon={<AddIcon />}>Create</Button>
-                </div>
-            </form>
-        </div>
+      <div>
+        {error && <div style={{ color: "red" }}>{error}</div>}
+        {success && <div style={{ color: "green" }}>{success}</div>}
+        <form onSubmit={handleSubmit}>
+          <div className="input-container">
+            <div>
+              <input
+                type="text"
+                value={courseCode}
+                onChange={(e) => setCourseCode(e.target.value)}
+                placeholder="Course Code"
+                required
+                className="input-field"
+              />
+            </div>
+            <div>
+              <input
+                type="text"
+                value={courseYear}
+                onChange={(e) => setCourseYear(e.target.value)}
+                placeholder="Year"
+                required
+                className="input-field"
+              />
+            </div>
+            <div>
+              <input
+                type="text"
+                value={courseSemester}
+                onChange={(e) => setCourseSemester(e.target.value)}
+                placeholder="Semester"
+                required
+                className="input-field"
+              />
+            </div>
+            <Button type="submit" variant="outlined" startIcon={<AddIcon />}>
+              Create
+            </Button>
+          </div>
+        </form>
+      </div>
     </>
-    );
+  );
 }
 
 export default CreateDelivery;
